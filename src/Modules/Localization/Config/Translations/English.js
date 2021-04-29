5ffd3542874478030387d1f9c675a1ac7d2b5a7f
beyzanurmakara <beyzamakara@gmail.com>
2021-04-29 17:25:39 +0300

    localization

--- src/Modules/Localization/Config/Translations/English.js
+++ src/Modules/Localization/Config/Translations/English.js
@@ -1,3 +1,25 @@
+import { Texts } from '../TextNames';
 export default {
-    hello: 'Hello',
+    [Texts.email]:'E-mail',
+    [Texts.password]:'Password',
+    [Texts.login]:'Login',
+    [Texts.signUp]:'Sign Up',
+    [Texts.username]:'Username',
+    [Texts.homePage]:'Home Page',
+    [Texts.settings]:'Settings',
+    [Texts.addNew]:'Add New',
+    [Texts.add]:'Add',
+    [Texts.shoppingType]:'Shopping Type',
+    [Texts.date]:'Date',
+    [Texts.price]:'Price',
+    [Texts.explanation]:'Explanation',
+    [Texts.delete]:'Delete',
+    [Texts.colorTheme]:'Color Theme',
+    [Texts.dark]:'Dark',
+    [Texts.light]:'Light',
+    [Texts.language]:'Language',
+    [Texts.english]:'English',
+    [Texts.turkish]:'Türkçe',
+    [Texts.signOut]:'Sign Out',   
 };
+
